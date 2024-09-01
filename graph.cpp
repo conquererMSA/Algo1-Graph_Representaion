b00041afb26c1b3002afe4ac171b5e5df2adb9e4
asus test <msa-99@outlook.com>
2024-09-01 14:45:56 +0600

    graph basic

---
+++ graph.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node* next=NULL;
+    Node(int val){
+        this->val=val;
+        this->next=NULL;
+    }
+};
+int main(){
+    
+    return 0;
+}
