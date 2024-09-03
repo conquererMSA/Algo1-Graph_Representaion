2d121a808ce63e6d278414dbbbbfc60f87bbbc12
asus test <msa-99@outlook.com>
2024-09-03 12:22:21 +0600

    adjacency list explained

---
+++ practice.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> v[10];
+int main(){
+    int totalNodes, totalEdges;
+    cin>>totalNodes>>totalEdges;
+    while(totalEdges--){
+        int a,b;
+        //ekhane a and b er value 0-5 ( v[0,1,2,3,4,5] ) porzonto hobe. v er index ekekta node ke represent kore. v er ekekta index abar ekta kore vector hold kore, ei vector e oi node/index er sathe directly connected node guluke store kore.
+        cin>>a,b;
+
+    }
+    return 0;
+}
