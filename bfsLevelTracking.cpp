b04a0ea20576a2aa0744e9995633072631af834a
asus test <msa-99@outlook.com>
2024-09-03 13:41:36 +0600

    bfs level track explained

---
+++ bfsLevelTracking.cpp
@@ -0,0 +1,103 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> v[10]; //eta "adjacecncy list"
+bool visited[10]; //kuno node visit hole tar value true hobe. ekhane index hocce ekekta node.
+int level[10];//proti node/index er initial level hobe -1. sorbucco level hobe node er sob ceye boro valur soman. visited na hole level re-assign hobe ba level barbe.
+void bfs(int srcNode){
+    memset(visited,false, sizeof(visited));
+    memset(level,-1, sizeof(level));
+    queue<int> q;
+    q.push(srcNode); //0
+    visited[srcNode]=true;
+    level[srcNode]=0;
+    while(!q.empty()){
+        int parent=q.front(); //parent node
+        cout<<"parent "<<parent<<" level "<<level[parent]<<endl;
+        q.pop();
+    //"adjacency list" theke child gulu niye aste hobe. queue theke parent ber kore ana hoyeche.
+       for(auto child:v[parent]){ //"adjacency list"theke parent er protiti child loop caliye ber kora ana hocce
+        cout<<child<<" level "<<level[child]<<endl;
+        if(visited[child]==false){ //visited array te zodi false thake tahole visit kora hoy ni. visit false hole kaj korbo.
+          q.push(child);
+          visited[child]=true;
+          level[child]=level[parent]+1; //visited na hole parent er level er sathe 1 add hobe. ar zodi visited hoy tahole tar level count kora hoye geche.
+        }
+        }
+    }
+}
+int main(){
+    int totalNodes, totalEdges;
+    cin>>totalNodes>>totalEdges;
+    while(totalEdges--){
+        int a,b;
+        //ekhane a and b er value 0-5 ( v[0,1,2,3,4,5] ) porzonto hobe. v er index ekekta node ke represent kore. v er ekekta index abar ekta kore vector hold kore, ei vector e oi node/index er sathe directly connected node guluke store kore.
+        cin>>a>>b;
+        v[a].push_back(b);
+        v[b].push_back(a);
+         // ekhane v ekti array zar index 0-5 porzonto. and a,b er value 0-5 porzontoi hobe orthat v array er kuno na kuno index er soman. dhori a=0, b=1; tahole v[0/a] ekti array er moddhye a node era sathe connected node b push kora hoyeche. ekoivabe b er sathe a connected howyay v[1/b] te a node push kora hoyeche.
+    }
+    int srcNode; //ze node theke traverse shuru hobe
+    cin>>srcNode;
+    bfs(srcNode); //BFS algo level order e kaj kore.
+    //print level from level array.
+    cout<<"nodes level print"<<endl;
+    for(int i=0; i<10; i++){
+        //level array er protita index e hocce ekekta node ar index value hocce tar level;
+        //i hocce node
+        int lebel=level[i];
+        cout<<i<<" level "<<lebel<<endl;
+    }
+    return 0;
+}
+/*
+input
+6 9
+0 1
+1 5
+2 3
+3 5
+1 3
+4 0
+0 2
+1 4
+4 5
+0
+
+output
+parent 0 level 0
+1 level -1
+4 level -1
+2 level -1
+parent 1 level 1
+0 level 0
+5 level -1
+3 level -1
+4 level 1
+parent 4 level 1
+0 level 0
+1 level 1
+5 level 2
+parent 2 level 1
+3 level 2
+0 level 0
+parent 5 level 2
+1 level 1
+3 level 2
+4 level 1
+parent 3 level 2
+2 level 1
+5 level 2
+1 level 1
+nodes level print
+0 level 0
+1 level 1
+2 level 1
+3 level 2
+4 level 1
+5 level 2
+6 level -1
+7 level -1
+8 level -1
+9 level -1
+
+*/
